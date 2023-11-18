b8d8203e659ee00b642610c5989a273c718aa0d2
Paul Antoni <paul.antoni@siemens-healthineers.com>
2023-11-18 16:15:46 +0100

    feat: add demo plot to gathered trends data

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from data_grabber import *
 import pandas
+import matplotlib.pyplot as plt
 
 data_fetcher = Data_grabber()
 data = data_fetcher.get_trends_data("cats")
+data.plot()
+
+
+plt.show()
