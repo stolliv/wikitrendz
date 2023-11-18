58c421179aad31dbc1e58be828657cf0c7f0532b
Paul Antoni <paul.antoni@siemens-healthineers.com>
2023-11-18 18:23:39 +0100

    feat: start implementing wikipedia api

--- data_grabber.py
+++ data_grabber.py
@@ -1,5 +1,6 @@
 from pytrends.request import TrendReq
 import pandas as pd
+import requests
 
 class Data_grabber:
     def __init__(self):
@@ -12,5 +13,19 @@ class Data_grabber:
         interest_over_time = self.pytrends.interest_over_time()
         return interest_over_time
 
-    def get_wiki_data(self):
-        pass
+    def get_wiki_data(self, keyword:str):
+        session = requests.Session()
+        request_url = "https://en.wikipedia.org/w/api.php"
+        parameters = {
+            "action": "query",
+            "prop": "revisions",
+            "titles": keyword,
+            "rvprop": "timestamp",#"timestamp|user|comment|content"
+            "rvslots": "main",
+            "formatversion": "2",
+            "format": "json"
+        }
+
+        request = session.get(url=request_url, params=parameters)
+        data = request.json()
+        pages = data["query"]["pages"] # TODO: Currently only one revision is returned, fix this
--- main.py
+++ main.py
@@ -7,5 +7,6 @@ data_fetcher = Data_grabber()
 data = data_fetcher.get_trends_data("cats")
 data.plot()
 
+data_fetcher.get_wiki_data("cat")
 
 plt.show()
